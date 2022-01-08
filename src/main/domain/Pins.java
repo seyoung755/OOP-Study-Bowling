274e43399855f6ef35c57288b035f1fa6131a7d3
seyeong <seyoung7555@naver.com>
2022-01-08 23:46:12 +0900

    Pins 클래스 작성 및 테스트 코드 작성
    - 볼링 핀 쓰러지는 기능 구현 및 테스트 완료

--- src/main/domain/Pins.java
+++ src/main/domain/Pins.java
@@ -1,7 +1,19 @@
 package main.domain;
 
-public class BowlingPins {
+public class Pins {
 
-    private final int MAX_BOWLING_PINS = 10;
-    
+    private final static int MAX_BOWLING_PINS = 10;
+    private int bowlingPins;
+
+    public Pins() {
+        this.bowlingPins = MAX_BOWLING_PINS;
+    }
+
+    public void rollingBall() {
+        int randomNumber = (int) (Math.random() * MAX_BOWLING_PINS);
+        hitBowlingPin(randomNumber);
+    }
+    private void hitBowlingPin(int hitBowlingPinCount) {
+        this.bowlingPins -= hitBowlingPinCount;
+    }
 }
--- src/test/PinsTest.java
+++ src/test/PinsTest.java
@@ -1,2 +1,28 @@
-package test;public class PinsTest {
+package test;
+
+import main.domain.Pins;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PinsTest {
+
+    @Test
+    public void 볼링핀이_쓰러진다() throws Exception {
+        Pins pins = new Pins();
+        int hitPinCount = 5;
+        
+        Method method = Pins.class.getDeclaredMethod("hitBowlingPin", int.class);
+        method.setAccessible(true);
+        method.invoke(pins, hitPinCount);
+
+        Field field = Pins.class.getDeclaredField("bowlingPins");
+        field.setAccessible(true);
+
+        int resultPin = (int) field.get(pins);
+        assertEquals(resultPin, 10 - hitPinCount);
+    }
 }
