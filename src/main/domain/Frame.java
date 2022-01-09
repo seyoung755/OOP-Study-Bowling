43cea042d774993905a3fd910c1e86acafb06a0e
seyeong <seyoung7555@naver.com>
2022-01-09 14:44:57 +0900

    ADD : Frame 객체 초기 구성

--- src/main/domain/Frame.java
+++ src/main/domain/Frame.java
@@ -2,8 +2,29 @@ package main.domain;
 
 public class Frame {
 
-    private static final int MAX_FRAME_NUMBER = 10;
+    private static final int TOTAL_FRAME = 10;
     private static final int NORMAL_FRAME_TURN = 2;
     private static final int FINAL_FRAME_TURN = 3;
-    
+    private static final int TOTAL_PIN = 10;
+
+    private int turn;
+
+    public Frame(int frameNumber) {
+        turn = NORMAL_FRAME_TURN;
+        if (frameNumber == TOTAL_FRAME) {
+            turn = FINAL_FRAME_TURN;
+        }
+    }
+
+    private void calculateScore(int pinCount) {
+        if (pinCount == TOTAL_PIN) {
+            turn -= 2;
+        }
+        turn--;
+    }
+
+    private boolean hasTurn() {
+        return turn > 0;
+    }
+
 }
